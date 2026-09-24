82b5d91bc0bfaed2dced49b2cd3252c69f19cfc5
vamsi-l4 <krishnavamsi3400@gmail.com>
2026-09-24 21:18:28 +0530

    search_rotated_sorted_array.py

--- day13_logic/search_rotated_sorted_array.py
+++ day13_logic/search_rotated_sorted_array.py
@@ -1,10 +1,21 @@
 numbers = list(map(int, input("Enter rotated sorted numbers: ").split()))
-
 target = int(input("Enter target: "))
-
 left = 0
 right = len(numbers) - 1
-
 while left <= right:
-
     middle = (left + right) // 2
+    if numbers[middle]==target:
+        print("found",middle)
+        break
+    if numbers[left]<=numbers[middle]:
+        if numbers[left]<=target<numbers[middle]:
+            right=middle-1
+        else:
+            left=middle+1
+    else:
+        if numbers[middle]<=target<numbers[right]:
+            left=middle+1
+        else:
+            right=middle-1
+else:
+    print("not found")             
